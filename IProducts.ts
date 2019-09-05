92ce160665850866510a4f13404ac9ea92e04e3d
Ankur Nigam <ankur.nigam198@gmail.com>
2019-09-05 11:58:20 +0530

    Added profuct images

--- IProducts.ts
+++ IProducts.ts
@@ -2,11 +2,15 @@ export interface IVariant {
   price: number;
   weight: number;
 }
-
+export type ProductType='tea'|'coffee';
 export interface IProduct {
+  productIconURL: String 
   name: String;
   summary: String;
   additionalDescription: String;
+  bigDescription:String
   ingredients: String[];
   variant: IVariant[];
+  type:ProductType
+  productImagesURL:String[]
 }
