266728f35b3c4bbd2ec316da24c6e15ed0a5562c
Ankur Nigam <ankur.nigam198@gmail.com>
2019-09-07 18:06:03 +0530

    String to string

--- IProducts.ts
+++ IProducts.ts
@@ -1,16 +1,22 @@
 export interface IVariant {
   price: number;
   weight: number;
+  quantity:number;
+  productImagesURL1:string  
+  productImagesURL2:string
 }
 export type ProductType='tea'|'coffee';
 export interface IProduct {
-  productIconURL: String 
-  name: String;
-  summary: String;
-  additionalDescription: String;
-  bigDescription:String
-  ingredients: String[];
+  productIconURL: string 
+  name: string;
+  summary: string;
+  additionalDescription: string;
+  bigDescription:string
+  ingredients: string[];
   variant: IVariant[];
   type:ProductType
-  productImagesURL:String[]
+  productImagesURL1:string
+  productImagesURL2:string
+  productImagesURL3:string
+  productImagesURL4:string
 }
